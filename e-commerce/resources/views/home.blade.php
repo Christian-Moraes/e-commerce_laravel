2fa7d1e9471c8f272981d3138598b2c8b3ba1427
Christian Moraes <christianmath2001@gmail.com>
2022-11-24 21:54:13 -0300

    first commit

---
+++ e-commerce/resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Shop - Ecommerce</title>
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
+
+</head>
+<body>
+  <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+    <a href="#">Shop</a>
+    <div class="collapse navbar-collapse">
+      <a class="nav-link" href="#">Home</a>
+      <a class="nav-link" href="#">Categorias</a>
+      <a class="nav-link" href="#">Cadastrar</a>
+    </div>
+    <a href="" class="btn btn-sm"><i class="fa fa-shopping-cart"></i></a>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-3 mb-3">
+        <div class="card">
+          <img src="../../imagens/arroz.jpg"  class="card-img-top" />
+          <div class="card-body">
+            <h6 class="card-title">Produto</h6>
+            <a href="" class="btn btn-sm btn-secondary">Adicionar item</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
